5c2a80f9fead95be0f24f02711615e6a7b4f921c
Lupe <lupemaydana@gmail.com>
2022-10-13 11:02:52 -0500

    chore: Add test files

---
+++ README.md
@@ -0,0 +1,37 @@
+# Reto#2 Repara los tests
+
+En este reto vas a encontrar tests que en este momento no funcionan, tu misión repararlos para que vuelvan a funcionar
+
+- [Instalación](#instalación)
+- [Pruebas](#pruebas)
+- [Como enviar tu solución](#como-enviar-tu-solución)
+- [Licencia](#licencia)
+- [Credits](#credits)
+
+## Instalación
+
+1. Hacer el fork de este proyecto en tu espacio personal
+1. Clonar el repositorio desde tu espacio personal en tu computadora
+1. Instalar dependencias, con el comando `npm install`
+
+## Pruebas
+
+Las pruebas end to end corren bajo playwright con el comando `npx playwright test` y ya esta incluido como parte de las dependencias del proyecto, sin embargo, antes de correr el comando, asegúrate de correr `npm install` para instalar los requerimientos de playwright para correr pruebas.
+
+Cuando corras el comando `npx playwright test` por primera vez las pruebas van a fallar y se verán así:
+
+![failed](./screenshots/failed.png)
+
+Una vez repares todos los tests deberías pasar todas las pruebas y se verán así:
+
+![success](./screenshots/success.png)
+
+> Puedes ir corriendo el comando `npx playwright test` de las funcionalidades que vayas creando para ir evaluando que vas por buen camino.
+
+## Como enviar tu solución
+
+Debes de hacer un "Fork" de este proyecto, revolver los problemas y crear un Pull Request hacia este repositorio.
+
+## Licencia
+
+Este proyecto se lanza bajo la licencia [MIT](https://opensource.org/licenses/MIT).
---
+++ tests/search.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
+
+test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
+  await page.getByRole('button').click();
+
+  await page.getByPlaceholder('Search docs').click();
+
+  await page.getByPlaceholder('Search docs').fill('hascontent');
+
+  expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
+
+  expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for hascontent');
+
+})
+
+test('Limpiar el input de busqueda', async ({ page }) => {
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  const searchBox = page.getByPlaceholder('Search docs');
+
+  await searchBox.click();
+
+  await searchBox.fill('somerandomtext');
+
+  await expect(searchBox).toHaveText('somerandomtext');
+
+  await page.getByRole('button', { name: 'Clear the query' }).click();
+
+  await expect(searchBox).toHaveAttribute('value', '');
+});
+
+test('Realizar una busqueda que genere al menos tenga un resultado', async ({ page }) => {
+  await page.getByRole('button', { name: 'Search ' }).click();
+
+  const searchBox = page.getByPlaceholder('Search docs');
+
+  await searchBox.click();
+
+  await page.getByPlaceholder('Search docs').fill('havetext');
+
+  expect(searchBox).toHaveText('havetext');
+
+  // Verity there are sections in the results
+  await page.locator('.DocSearch-Dropdown-Container section').nth(1).waitFor();
+  const numberOfResults = await page.locator('.DocSearch-Dropdown-Container section').count();
+  await expect(numberOfResults).toBeGreaterThan(0);
+
+});
